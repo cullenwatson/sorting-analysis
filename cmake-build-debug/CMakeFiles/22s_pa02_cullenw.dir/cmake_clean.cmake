866b2888b72277d9384d0cbfc26813905b81fe49
cgwatson193 <78247585+cgwatson193@users.noreply.github.com>
2022-03-02 11:42:58 -0600

    quicksort implemented for ints

---
+++ cmake-build-debug/CMakeFiles/22s_pa02_cullenw.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "22s_pa02_cullenw"
+  "22s_pa02_cullenw.pdb"
+  "CMakeFiles/22s_pa02_cullenw.dir/src/Algo.cpp.o"
+  "CMakeFiles/22s_pa02_cullenw.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/22s_pa02_cullenw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
